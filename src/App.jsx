5d63a33b6ca061bdd2f6de8556f4c7a0fc4cd53f
Mariia <mariadufanets06@gmail.com>
2024-04-19 20:41:12 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,32 @@ import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
 import "./App.css";
 import ContactForm from "./components/ContactForm/ContactForm";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchContacts } from "./redux/contactsOperations";
+import { selectError, selectIsLoading } from "./redux/contactsSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div>
+    <div className="container">
       <h1>Phonebook</h1>
       <ContactForm />
       <SearchBox />
+      {isLoading && !error && <b>Request in progress...</b>}
+      {error && (
+        <b>
+          There is a problem with the connection to the server, please try again
+          later
+        </b>
+      )}
       <ContactList />
     </div>
   );
